183a737cd05f484683a7354da8643572f344667e
John <johnconley507@hotmail.com>
2017-06-12 18:41:43 -0400

    working with control structures and xss proctection

--- getting-started/resources/views/welcome.blade.php
+++ getting-started/resources/views/welcome.blade.php
@@ -3,8 +3,20 @@
 @section('content')
 	<div class="row">
 		<div class="col-md-12">
-			<h1>Some Content</h1>
-			<p>{{ "Hello" }}</p>
+			<h1>Control Structures</h1>
+				@if(true)
+					<p>This only displays if it is true.</p>
+				@else
+					<p>This only displays if it is false</p>
+				@endif
+				<hr>
+				@for($i = 0; $i < 5; $i++)
+					<p>{{ $i + 1 }}. Iteration</p>
+				@endfor
+				<hr>
+				<h2>XSS</h2>
+				{{ "<script>alert('Hello');</script>"}}
+				{!!"<script>alert('Hello');</script>"!!}
 		</div>
 	</div>
 

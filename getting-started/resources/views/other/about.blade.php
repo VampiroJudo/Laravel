b70aa2bc5119b195c4663feb4a1952134d2d984b
John <johnconley507@hotmail.com>
2017-06-13 19:52:17 -0400

    adding dummy text to 'about' view

---
+++ getting-started/resources/views/other/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content')
+	<div class="row">
+		<div class="col-md-12">
+			<p class="quote">About Me</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<p>Lorem ipsum dolor etc etc</p>
+		</div>
+	</div>
+@endsection

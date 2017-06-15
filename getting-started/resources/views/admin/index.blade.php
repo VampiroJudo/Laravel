22f5ca3773b70eb9ad7cc37f0178900a332585b6
John <johnconley507@hotmail.com>
2017-06-15 13:46:12 -0400

    adding routes to views

--- getting-started/resources/views/admin/index.blade.php
+++ getting-started/resources/views/admin/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 	<div class="row">
 		<div class="col-md-12">
-			<a href="" class="btn btn success">New Post</a>
+			<a href="{{ route('admin.create') }}" class="btn btn success">New Post</a>
 		</div>
 	</div>
 	<hr>
 	<div class="row">
 		<div class="col-md-12">
-			<p><strong>Learning Laravel</strong><a href="">Edit</p>
+			<p><strong>Learning Laravel</strong><a href="{{ route('admin.edit') }} ">Edit</p>
 		</div>
 	</div>
 @endsection
--- getting-started/resources/views/partials/admin-header.blade.php
+++ getting-started/resources/views/partials/admin-header.blade.php
@@ -1,9 +1,10 @@
 <nav class="navbar navbar-default">
 	<div class="container-fluid">
 		<div class="navbar-header">
-			<a class="navbar-brand" href="">Laravel Guide</a>
+			<a class="navbar-brand" href="{{ route('blog.index')}}">Laravel Guide</a>
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="">Posts</a></li>
+				<li class="active"><a href="{{ route('blog.index')}}">Posts</a></li>
+				<li><a href="{{ route('other.about')}}"></a></li>
 			</ul>
 		</div>
 	</div>

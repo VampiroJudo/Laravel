c4c397db44e7f63ea92f21910c22de2aafec3402
John <johnconley507@hotmail.com>
2017-06-19 17:48:37 -0400

    adding CSRF protection to forms that post

--- getting-started/routes/web.php
+++ getting-started/routes/web.php
@@ -32,7 +32,7 @@ Route::group(['prefix' => 'admin'], function() {
 		return view('admin.create');
 	})->name('admin.create');
 
-	Route::post('create', function() {
+	Route::post('create', function(\Illuminate\Http\Request $request) {
 		return('It works!')
 	})->name('admin.create');
 
@@ -51,7 +51,7 @@ Route::group(['prefix' => 'admin'], function() {
 		return view('admin.edit');
 	})->name('admin.edit');
 
-	Route::post('edit', function() {
+	Route::post('edit', function(\Illuminate\Http\Request $request) {
 		return('It works!')
 	})->name('admin.update');
 });
